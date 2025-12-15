f74d58930d6c22aa90326f2373c7a39050429e27
CountZero <artemstbad@gmail.com>
2025-12-15 15:13:20 +0300

    + level

--- main.cpp
+++ main.cpp
@@ -13,10 +13,19 @@ class Employee{
         Employee(string name_, int id_, double salary_): name(name_), id(id_), salary(salary_) {}
         string getName() const {return name;}
         int getId() const {return id;}
-        double getSalary() const {return salary;}
+        virtual double getSalary() const {return salary;}
         void setSalary(double newSalary) {salary = newSalary;}  
 };
 
+class Manager: public Employee {
+    private:
+        string department;
+    public:
+        Manager(string name_, int id_, double salary_, string department_): 
+        Employee(name_, id_, salary_), department(department_) {}
+        double getSalary() const {return Employee::getSalary() * 1.1;}
+        string getDepartment() const {return department;}
+};
 bool isValidEmployeeData(const string& name, int id, double salary){
     return !name.empty() && id > 0 && salary >= 0;
 }
@@ -36,6 +45,9 @@ void saveToFile(const Employee employees[], int size, const string& filename){
     outfile.close();
 }
 int main(){
+    // Basic level
+
+    {
     Employee employees[5];
     for (int i = 0; i < 3; i++){
         cout << "Input " << i + 1 << " employee data.\n";
@@ -65,5 +77,25 @@ int main(){
         }
     }
     saveToFile(employees, 5, "employees.txt");
+    }
+
+    // *
+
+    {
+        Employee* employees[4];
+        Employee em1("John", 1, 1000);
+        Employee em2("Bob", 2, 1000);
+        Manager em3("Victor", 3, 5000, "PZ");
+        Manager em4("Saul Goodman", 4, 10000, "Lawyer");
+        employees[0] = &em1;
+        employees[1] = &em2;
+        employees[2] = &em3;
+        employees[3] = &em4;
+        for (int i = 0; i < 4; i++){
+            string name = employees[i]->getName();
+            double salary = employees[i]->getSalary();
+            cout << "Salary " << name << ": " << salary << '\n';
+        }
+    }
     return 0;
 }
